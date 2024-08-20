e21a18488a608f19e5fb63daa1108929c2da5583
Keigo Hachisuka <keigoh@keigos-mbp-2.mynetworksettings.com>
2024-08-19 21:11:09 -0400

    about update

--- src/components/About.js
+++ src/components/About.js
@@ -1,30 +1,54 @@
-import { TheLinks } from "./Links"
-import '../component-css/about.css'
+import { TheLinks } from "./Links";
+import "../component-css/about.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import personal from '../img/personal.png'
+import personal from "../img/personal.png";
 
 export const About = () => {
-    return (
-      <div>
-        <div className='about-links'>
-          <TheLinks curLink='about'/>
-        </div>
-        <div className="about-outer-container">
-          <div className="about-container">
-              <h1 className='about-me'>about me</h1>
-              <div className="text-img">
-              <p className="about-me-text">Recent graduate from Brown University with a degree in Computer Science and Applied Mathematics, seeking to advance skills in software engineering. Proficient in Java, Python, and C. Continuously working on enhancing problem-solving abilities applicable within and beyond software engineering.</p>
-              <img className="my-img" src={personal} width="200" height="300" alt="personal"/>
-              </div>
-            <div className="icons">
-              <a rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/keigo-hachisuka-a91523188/"><FontAwesomeIcon  className="icon" icon={faLinkedin} /></a>
-              <a rel="noopener noreferrer" target="_blank" href="https://github.com/keigo-h"><FontAwesomeIcon className="icon" icon={faGithub} /></a>
-              <a href="mailto:keigo.hachisuka01@gmail.com"><FontAwesomeIcon className="icon" icon={faEnvelope} /></a>
-            </div>
+  return (
+    <div>
+      <div className="about-links">
+        <TheLinks curLink="about" />
+      </div>
+      <div className="about-outer-container">
+        <div className="about-container">
+          <h1 className="about-me">about me</h1>
+          <div className="text-img">
+            <p className="about-me-text">
+              Recent Brown University grad. <br />
+              Currently a software engineer at Hurdlr.
+            </p>
+            {/* Recent graduate from Brown University with a degree in Computer Science and Applied Mathematics, seeking to advance skills in software engineering. Proficient in Java, Python, and C. Continuously working on enhancing problem-solving abilities applicable within and beyond software engineering */}
+            <img
+              className="my-img"
+              src={personal}
+              width="200"
+              height="300"
+              alt="personal"
+            />
+          </div>
+          <div className="icons">
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://www.linkedin.com/in/keigo-hachisuka-a91523188/"
+            >
+              <FontAwesomeIcon className="icon" icon={faLinkedin} />
+            </a>
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://github.com/keigo-h"
+            >
+              <FontAwesomeIcon className="icon" icon={faGithub} />
+            </a>
+            <a href="mailto:keigo.hachisuka01@gmail.com">
+              <FontAwesomeIcon className="icon" icon={faEnvelope} />
+            </a>
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  );
+};
