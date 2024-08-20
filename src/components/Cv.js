c82dba1bf1481a4732fa7940489cf2c239a8a372
Keigo Hachisuka <keigoh@keigos-mbp-2.mynetworksettings.com>
2024-08-19 21:00:39 -0400

    updates to cv

--- src/components/Cv.js
+++ src/components/Cv.js
@@ -1,13 +1,11 @@
-import { TheLinks } from "./Links"
-import '../component-css/cv.css'
-import { faChevronDown } from "@fortawesome/free-solid-svg-icons/faChevronDown"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { useEffect, useRef } from "react"
-import { useLocation } from "react-router-dom"
+import { TheLinks } from "./Links";
+import "../component-css/cv.css";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons/faChevronDown";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useRef } from "react";
+import { useLocation } from "react-router-dom";
 
-
-export const Cv =  () => {
-
+export const Cv = () => {
   const { curLoc } = useLocation();
 
   useEffect(() => {
@@ -18,42 +16,59 @@ export const Cv =  () => {
 
   const scroll = () => {
     if (topExp.current) {
-      topExp.current.scrollIntoView({ behavior: 'smooth', block: 'center' });
+      topExp.current.scrollIntoView({ behavior: "smooth", block: "center" });
     }
   };
 
-    return (
-      <div>
-        <div className="cv-links">
-          <TheLinks curLink='cv'/>
+  return (
+    <div>
+      <div className="cv-links">
+        <TheLinks curLink="cv" />
+      </div>
+      <div className="cv-outer-container">
+        <div className="cv-container">
+          <h1 className="cv" onClick={scroll}>
+            CV
+          </h1>
         </div>
-        <div className="cv-outer-container">
-          <div className="cv-container">
-            <h1 className='cv' onClick={scroll}>CV</h1>
-          </div>
-          <div className="cv-icon" onClick={scroll}>
-              <FontAwesomeIcon icon={faChevronDown} />
-          </div>
-        </div>
-        <div ref={topExp} className="all-exp-wrapper">
-          <h2 className='cv-sub'>experience</h2>
-          <Exp title="Proofpoint Software Engineer Intern" />
-          <Exp title="Lawrence Livermore National Lab Cybertoaster Intern" />
-          <h2 className='cv-sub'>education</h2>
-          <Exp title="Brown University" />
+        <div className="cv-icon" onClick={scroll}>
+          <FontAwesomeIcon icon={faChevronDown} />
         </div>
       </div>
-    )
-  }
+      <div ref={topExp} className="all-exp-wrapper">
+        <h2 className="cv-sub">experience</h2>
+        <Exp
+          title="Hurdlr"
+          date={"August 2024-Present"}
+          position={"Software Engineer"}
+        />
+        <Exp
+          title="Proofpoint"
+          date={"May 2023-August 2023"}
+          position={"Software Engineer Intern"}
+        />
+        <Exp
+          title="Lawrence Livermore National Lab"
+          date={"June 2022-August 2022"}
+          position={"Cybertoaster Intern"}
+        />
+        <h2 className="cv-sub">education</h2>
+        <Exp title="Brown University" date={"2020-2024"} position={"BS Computer Science, BA Applied Mathematics"} />
+      </div>
+    </div>
+  );
+};
 
-const Exp = ({title}) => {
+const Exp = ({ title, date, position }) => {
   return (
     <div className="ind-exp-wrapper">
-      <h3 className="cv-text">{title}</h3>
+      <div className="cv-title-wrapper">
+        <h3 className="cv-text">{title}</h3>
+        <span className="cv-date">{date}</span>
+      </div>
       <p className="cv-des">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at euismod ipsum. Suspendisse lorem felis, venenatis eu est eget, condimentum placerat eros. 
+        {position}
       </p>
     </div>
-  )
-}
-
+  );
+};
